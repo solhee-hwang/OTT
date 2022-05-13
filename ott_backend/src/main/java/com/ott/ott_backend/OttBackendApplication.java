27de97c85a7b50e58b310b44be16c1982e5b17e4
solhee-hwang <solhee0912@kookmin.ac.kr>
2022-05-13 22:29:28 +0900

    feat : Add backend

---
+++ ott_backend/src/main/java/com/ott/ott_backend/OttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ott.ott_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttBackendApplication.class, args);
+	}
+
+}
