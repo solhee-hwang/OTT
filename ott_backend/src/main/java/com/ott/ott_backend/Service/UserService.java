3d742e34490fa6bd3a67292f2720a31340c8cb75
solhee-hwang <solhee0912@kookmin.ac.kr>
2022-05-20 15:02:04 +0900

    fix : Find object with autowired

--- ott_backend/src/main/java/com/ott/ott_backend/Service/UserService.java
+++ ott_backend/src/main/java/com/ott/ott_backend/Service/UserService.java
@@ -3,12 +3,15 @@ package com.ott.ott_backend.Service;
 import com.ott.ott_backend.model.UserEntity;
 import com.ott.ott_backend.persistence.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class UserService {
+    @Autowired
     private UserRepository userRepository;
+
     public UserEntity create(final UserEntity userEntity){
         if(userEntity == null || userEntity.getEmail() == null){
             throw new RuntimeException("Invalid arguments");
